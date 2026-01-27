5315dc9d368c8ca3247fbf8276a13a7f36e3e1b5
Justin C <tulare789@gmail.com>
2026-01-27 18:02:37 -0500

    Removed dependancy on microsd, test outside

--- LightMeter2_BASIC.ino
+++ LightMeter2_BASIC.ino
@@ -1,5 +1,5 @@
 
-// library setup
+//library setup
 
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
@@ -17,14 +17,16 @@ Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591); // establishes a light sensor cod
 SoftwareSerial mySerial(11, 10);
 Adafruit_GPS GPS(&mySerial);
 
-#define GPSECHO true // sends data to Serial Monitor
 
-// setup code runs once
+#define GPSECHO  true     // sends data to Serial Monitor
+
+
+//setup code runs once
 void setup()
 {
-  Serial.begin(9600);                 // set baud rate to 115200
-  delay(1000);                          // wait
-  Serial.println("GPS SD LIGHT METER"); // print startup and fun dots
+  Serial.begin(9600); //set baud rate to 115200
+  delay(1000); //wait
+  Serial.println("GPS SD LIGHT METER"); //print startup and fun dots
   delay(200);
   Serial.print(".");
   delay(200);
@@ -32,56 +34,58 @@ void setup()
   delay(200);
   Serial.println(".");
 
-  Serial.println("SD.CHECK"); // begins check for SD card Reader
+/* TESTING: remove SD card dependancy
+  Serial.println("SD.CHECK"); //begins check for SD card Reader
 
-  if (!SD.begin(chipSelect))
-  {
+  if (!SD.begin(chipSelect)) {
     Serial.println("Card failed, or not present");
-    // dititalWrite(5) = HIGH; // if connnected at pin 5, an LED light turns on
-    while (1)
-      ; // don't do anything more:
+    //dititalWrite(5) = HIGH; // if connnected at pin 5, an LED light turns on
+    while (1); // don't do anything more:
   }
-  // if an SD card is not present, the program halts here.
-  // if an SD card is present, the program continues
+*/
+  //if an SD card is not present, the program halts here.
+  //if an SD card is present, the program continues
 
   Serial.println("SD.PASS");
 
+
   delay(5000);
+
 
   Serial.println("GPS.CHECK");
 
   GPS.begin(9600); // I honestly do not understand the complexity of the GPS parsing, but all of this is required
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
   GPS.sendCommand(PGCMD_ANTENNA);
   delay(1000);
   mySerial.println(PMTK_Q_RELEASE);
 
-  Serial.println("GPS.PASS"); // if all of this runs, it works!
+  Serial.println("GPS.PASS"); //if all of this runs, it works!
+
 
   Serial.println("LIGHT.SENSOR.CHECK");
 
-  if (tsl.begin())
-  {
+  if (tsl.begin()) {
     Serial.println(F("Found a TSL2591 sensor"));
   }
-  else
-  {
+  else {
     Serial.println(F("No sensor found ... check your wiring?"));
-    while (1)
-      ;
+    while (1);
   }
 
   Serial.println("SENSOR.PASS");
+
 
   Serial.println("SENSOR.CONFIG.CHECK");
   configureSensor();
   Serial.println("SENSOR.CONFIG.PASS");
 
-  Serial.println("AWAIT.FIX.FOR.LOOP.START"); // Prints to Serial, that Startup has ended
+
+  Serial.println("AWAIT.FIX.FOR.LOOP.START"); //Prints to Serial, that Startup has ended
 }
 
-// This code is a few functions that the rest of the loop runs.
+//This code is a few functions that the rest of the loop runs.
 
 void simpleRead(void)
 {
@@ -89,8 +93,8 @@ void simpleRead(void)
   // or 'visible' (difference between the two) channels.
   // This can take 100-600 milliseconds! Uncomment whichever of the following you want to read
   uint16_t x = tsl.getLuminosity(TSL2591_VISIBLE);
-  // uint16_t x = tsl.getLuminosity(TSL2591_FULLSPECTRUM);
-  // uint16_t x = tsl.getLuminosity(TSL2591_INFRARED);
+  //uint16_t x = tsl.getLuminosity(TSL2591_FULLSPECTRUM);
+  //uint16_t x = tsl.getLuminosity(TSL2591_INFRARED);
 }
 void advancedRead(void)
 {
@@ -107,80 +111,80 @@ void advancedRead(void)
   //  Serial.print(F("Lux: ")); Serial.println(tsl.calculateLux(full, ir), 6);
 }
 
-void configureSensor(void)
-{
-  // tsl.setGain(TSL2591_GAIN_LOW);    // 1x gain (bright light)
-  tsl.setGain(TSL2591_GAIN_MED); // 25x gain
-  // tsl.setGain(TSL2591_GAIN_HIGH);   // 428x gain
+void configureSensor(void) {
+  //tsl.setGain(TSL2591_GAIN_LOW);    // 1x gain (bright light)
+  tsl.setGain(TSL2591_GAIN_MED);      // 25x gain
+  //tsl.setGain(TSL2591_GAIN_HIGH);   // 428x gain
 
   // Changing the integration time gives you a longer time over which to sense light
   // longer timelines are slower, but are good in very low light situtations!
-  // tsl.setTiming(TSL2591_INTEGRATIONTIME_100MS);  // shortest integration time (bright light)
+  //tsl.setTiming(TSL2591_INTEGRATIONTIME_100MS);  // shortest integration time (bright light)
   // tsl.setTiming(TSL2591_INTEGRATIONTIME_200MS);
-  // tsl.setTiming(TSL2591_INTEGRATIONTIME_300MS);
+  //tsl.setTiming(TSL2591_INTEGRATIONTIME_300MS);
   // tsl.setTiming(TSL2591_INTEGRATIONTIME_400MS);
   tsl.setTiming(TSL2591_INTEGRATIONTIME_500MS);
-  // tsl.setTiming(TSL2591_INTEGRATIONTIME_600MS);  // longest integration time (dim light)
+  //tsl.setTiming(TSL2591_INTEGRATIONTIME_600MS);  // longest integration time (dim light)
 
   /* Display the gain and integration time for reference sake */
   Serial.println(F("------------------------------------"));
-  Serial.print(F("Gain:         "));
+  Serial.print  (F("Gain:         "));
   tsl2591Gain_t gain = tsl.getGain();
   switch (gain)
   {
-  case TSL2591_GAIN_LOW:
-    Serial.println(F("1x (Low)"));
-    break;
-  case TSL2591_GAIN_MED:
-    Serial.println(F("25x (Medium)"));
-    break;
-  case TSL2591_GAIN_HIGH:
-    Serial.println(F("428x (High)"));
-    break;
-  case TSL2591_GAIN_MAX:
-    Serial.println(F("9876x (Max)"));
-    break;
+    case TSL2591_GAIN_LOW:
+      Serial.println(F("1x (Low)"));
+      break;
+    case TSL2591_GAIN_MED:
+      Serial.println(F("25x (Medium)"));
+      break;
+    case TSL2591_GAIN_HIGH:
+      Serial.println(F("428x (High)"));
+      break;
+    case TSL2591_GAIN_MAX:
+      Serial.println(F("9876x (Max)"));
+      break;
   }
 }
 
 uint32_t timer = millis();
 
-void loop() // The meat and potatoes. This runs constantly after the above code has been processed.
+
+
+
+
+void loop()                     // The meat and potatoes. This runs constantly after the above code has been processed. 
 
 {
-  advancedRead();      // each loop we get a new light value.
-  char c = GPS.read(); // read the gps status
+  advancedRead(); //each loop we get a new light value. 
+  char c = GPS.read();//read the gps status
   if ((c) && (GPSECHO))
-    if (GPS.newNMEAreceived())
-    {
+    if (GPS.newNMEAreceived()) {
       if (!GPS.parse(GPS.lastNMEA()))
         return;
     }
-
-  if (millis() - timer > 2000)
-  {
+    
+  if (millis() - timer > 2000) {
     timer = millis(); // reset the timer
-    // we only want the meter to plot when there is a GPS fix, otherwise we can make it do something else by writing some code.
-    // there are other GPS parameters, like the number of satilites termed "quality". you can find the return code for that in the GPS testing file.
-    if (GPS.fix)
-    {
+    //we only want the meter to plot when there is a GPS fix, otherwise we can make it do something else by writing some code.
+    //there are other GPS parameters, like the number of satilites termed "quality". you can find the return code for that in the GPS testing file. 
+    if (GPS.fix) {
 
-      // light level read
+      //light level read
 
       uint32_t lum = tsl.getFullLuminosity();
       uint16_t ir, full;
       ir = lum >> 16;
       full = lum & 0xFFFF;
 
-      // establish a string of data to be put into CSV format. Strings are basically just excell files that the computer can reference.
+      // establish a string of data to be put into CSV format. Strings are basically just excell files that the computer can reference. 
 
       String dataString = "";
 
-      // date input
-      // the format for input to a particular string is type the string name.
-      // type up to the (
-      // type the varaible name OR any text in ""
-      // to create a CSV, you must Separate Values by Commas, so end by adding each field with a comma
+      //date input
+      //the format for input to a particular string is type the string name. 
+      //type up to the (
+      //type the varaible name OR any text in ""
+      //to create a CSV, you must Separate Values by Commas, so end by adding each field with a comma
 
       dataString += String(GPS.day);
       dataString += String("/");
@@ -189,7 +193,7 @@ void loop() // The meat and potatoes. This runs constantly after the above code 
       dataString += String(GPS.year);
       dataString += ",";
 
-      // time input
+      //time input
 
       dataString += String(GPS.hour);
       dataString += String(':');
@@ -198,38 +202,42 @@ void loop() // The meat and potatoes. This runs constantly after the above code 
       dataString += String(GPS.seconds);
       dataString += ",";
 
-      dataString += String(tsl.calculateLux(full, ir), 2); // references the function. I am not sure if it runs the function again to get this. Regardless, it works
+      dataString += String(tsl.calculateLux(full, ir), 2); //references the function. I am not sure if it runs the function again to get this. Regardless, it works
       dataString += ",";
 
-      File dataFile = SD.open("datalog.csv", FILE_WRITE); // this writes to a particular file on the SD card
-      // there can be multiple files set up on this, so if you would like to make a USER HISTORY, another file could write the start time and then overwrite an end time until the unit shuts off.
-      // then annother entry can start up when you begin.
+      Serial.println(dataString);
 
-      if (dataFile)
-      {
-        // print the string made above
+      /* TESTING: remove SD card dependancy
+      File dataFile = SD.open("datalog.csv", FILE_WRITE); // this writes to a particular file on the SD card
+      //there can be multiple files set up on this, so if you would like to make a USER HISTORY, another file could write the start time and then overwrite an end time until the unit shuts off. 
+      //then annother entry can start up when you begin. 
+
+      if (dataFile) {
+        //print the string made above
         dataFile.print(dataString);
-        // lat and lon must be printed directly to the SD file because of some dumb
-        // way the machine stores the number of decimal points.
+        //lat and lon must be printed directly to the SD file because of some dumb
+        //way the machine stores the number of decimal points.
         dataFile.print(GPS.latitude, 4);
         dataFile.print(",");
-        // separated by a comma
+        //separated by a comma
         dataFile.println(GPS.longitude, 4);
-        // NOT separated by a comma, but added a new line (nl)
-        // the 4 is for the number of decimal points
+        //NOT separated by a comma, but added a new line (nl)
+        //the 4 is for the number of decimal points
         dataFile.close();
         // print to the serial port too:
         Serial.print(dataString);
         Serial.print(GPS.latitude, 4);
         Serial.print(",");
         Serial.println(GPS.longitude, 4);
+
       }
       // if the file isn't open or available, pop up an error:
-      else
-      {
+      else {
         Serial.println("error opening DATALOG.csv");
       }
+      */
     }
-    // if there is no FIX, we can make the code do something here, hint:lights or sounds would go here
+    //if there is no FIX, we can make the code do something here, hint:lights or sounds would go here
+    
   }
 }
